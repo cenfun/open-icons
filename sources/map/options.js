367d349fe4a6866afb0cb0bfe27e7089d4f33297
cenfun <cenfun@qq.com>
2022-08-31 20:52:25 +0800

    add map-icons

---
+++ sources/map/options.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'map-icons',
+    url: 'https://github.com/scottdejonge/Map-Icons',
+    dirs: 'node_modules/map-icons/src/icons',
+
+    onSVGDocument: function($svg) {
+        let found = false;
+        ['g', 'path'].forEach((k) => {
+            const $elem = $svg.find(k);
+            const fill = $elem.attr('fill');
+            if (fill && fill !== 'none') {
+                $elem.attr('fill', 'currentColor');
+                found = true;
+            }
+        });
+        if (found) {
+            return;
+        }
+        $svg.attr('fill', 'currentColor');
+    }
+
+};
