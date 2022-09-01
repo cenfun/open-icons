41142421ffa8c68b44ccd23b4d8c468b2debdbb4
cenfun <cenfun@qq.com>
2022-09-01 13:19:20 +0800

    add @shopify/polaris-icons

--- sources/polaris/options.js
+++ sources/polaris/options.js
@@ -1,7 +1,6 @@
 const Helper = require('../../scripts/helper.js');
 
 module.exports = {
-    debug: true,
     name: '@shopify/polaris-icons',
     url: 'https://github.com/Shopify/polaris',
     dirs: 'node_modules/@shopify/polaris-icons/dist/svg',
