122c97323aaa77a01ae173ac87f5c41e6ec67a74
cenfun <cenfun@qq.com>
2022-09-05 15:23:20 +0800

    add paymentfont

---
+++ sources/payment/options.js
@@ -0,0 +1,34 @@
+
+const path = require('path');
+const SFE = require('svg-font-extractor');
+
+module.exports = {
+    debug: true,
+    name: 'paymentfont',
+    url: 'https://github.com/AlexanderPoellmann/PaymentFont',
+    dirs: function(Util) {
+        const modulePath = this.modulePath;
+
+        SFE({
+            input: path.resolve(modulePath, 'fonts/paymentfont-webfont.svg'),
+            output: path.resolve(modulePath, 'svg'),
+            onSVGItem: function(item) {
+                const name = item['glyph-name'];
+                if (!name || !item.d) {
+                    //console.log(item);
+                    return;
+                }
+
+                item.name = name.replace(/_/g, '-');
+                return item;
+            }
+        });
+
+        return 'svg';
+    },
+
+    onSVGDocument: function($svg) {
+        $svg.attr('fill', 'currentColor');
+    }
+
+};
