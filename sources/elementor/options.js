0131429da947e25bcd212033511b7c2b1d51caa8
cenfun <cenfun@qq.com>
2022-09-05 10:37:50 +0800

    add elementor-icons

---
+++ sources/elementor/options.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const SFE = require('svg-font-extractor');
+
+module.exports = {
+    name: 'elementor-icons',
+    url: 'https://github.com/elementor/elementor-icons',
+    download: {
+        url: 'https://github.com/elementor/elementor-icons/archive/refs/heads/master.zip',
+        handler: function(Util) {
+            const oldPath = path.resolve(this.sourcePath, 'elementor-icons-master');
+            const newPath = path.resolve(this.sourcePath, 'package');
+            fs.renameSync(oldPath, newPath);
+
+            //has package.json but no npm publish
+
+        }
+    },
+
+    dirs: function(Util) {
+        const modulePath = this.modulePath;
+
+        SFE({
+            input: path.resolve(modulePath, 'fonts/eicons.svg'),
+            output: path.resolve(modulePath, 'svg')
+        });
+
+        return 'svg';
+    },
+
+    onSVGDocument: function($svg) {
+        $svg.attr('fill', 'currentColor');
+    }
+};
