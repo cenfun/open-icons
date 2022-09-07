55490920d92da88a241658bcdf48cbdb5cc3b934
cenfun <cenfun@qq.com>
2022-09-07 23:30:01 +0800

    add coolicons

---
+++ sources/cool/options.js
@@ -0,0 +1,38 @@
+module.exports = {
+    name: 'coolicons',
+    url: 'https://github.com/krystonschwarze/coolicons',
+
+    downloadUrl: 'https://github.com/krystonschwarze/coolicons/archive/refs/heads/master.zip',
+    moduleEntry: 'coolicons-master',
+
+    moduleFilters: 'coolicons v2.5 SVG',
+
+    license: 'CC 4.0',
+
+    dirs: 'coolicons v2.5 SVG',
+
+    onSVGName: function(name, item) {
+        name = name.toLowerCase();
+
+        name = name.replace(/_/g, '-');
+
+        return this.onSVGNameDefault(name, item);
+    },
+
+    onSVGDocument: function($svg, item) {
+        let found = false;
+        ['path'].forEach((k) => {
+            const $elem = $svg.find(k);
+            const fill = $elem.attr('fill');
+            if (fill && fill !== 'none') {
+                $elem.attr('fill', 'currentColor');
+                found = true;
+            }
+        });
+        if (found) {
+            return;
+        }
+        $svg.attr('fill', 'currentColor');
+    }
+
+};
