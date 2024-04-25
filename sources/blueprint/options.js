35ac5d2360f193a632bd31fd9a6860647cd74006
cenfun <cenfun@gmail.com>
2024-04-25 17:04:12 +0800

    fix blueprint

--- sources/blueprint/options.js
+++ sources/blueprint/options.js
@@ -1,6 +1,5 @@
-const fs = require('fs');
 const path = require('path');
-const Helper = require('../../scripts/helper.js');
+const SFE = require('svg-font-extractor');
 
 module.exports = {
     name: '@blueprintjs/icons',
@@ -8,34 +7,14 @@ module.exports = {
 
     moduleInit: function(modulePath, Util) {
 
-        const dir = Util.createSvgDir();
-
-        const bundle = require(modulePath);
-        // console.log(bundle);
-
-        const keys = Object.keys(bundle);
-        // console.log(keys.length);
-
-        keys.forEach((k) => {
-
-            // console.log(k);
-            const Icon = bundle[k];
-            if (!Icon || !Icon.render) {
-                return;
-            }
-
-            const svg = Helper.renderReactIcon(Icon, {
-                size: 20
-            });
-
-            // console.log(svg);
-            if (!svg) {
-                return;
-            }
-
-            fs.writeFileSync(path.resolve(dir, `${Helper.pascalToKebabCase(k)}.svg`), svg);
-
+        SFE({
+            input: path.resolve(modulePath, 'lib/css/blueprint-icons-20.svg'),
+            output: path.resolve(modulePath, 'svg')
         });
 
+    },
+
+    onSVGDocument: function($svg) {
+        $svg.attr('fill', 'currentColor');
     }
 };
