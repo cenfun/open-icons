3e327f89f931e88d512f6fdb47f1e9f4134f96d3
cenfun <cenfun@gmail.com>
2024-04-25 18:10:39 +0800

    prime

--- sources/prime/options.js
+++ sources/prime/options.js
@@ -4,6 +4,19 @@ module.exports = {
     dirs: 'raw-svg',
 
     onSVGDocument: function($svg) {
+        let found = false;
+        ['circle', 'path'].forEach((k) => {
+            const $elem = $svg.find(k);
+            const fill = $elem.attr('fill');
+            if (fill && fill !== 'none') {
+                $elem.attr('fill', 'currentColor');
+                found = true;
+            }
+        });
+        if (found) {
+            return;
+        }
         $svg.attr('fill', 'currentColor');
     }
+
 };
