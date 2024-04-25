8610000eeb6ce80d0b96f874c146109454e5905e
cenfun <cenfun@gmail.com>
2024-04-25 18:13:46 +0800

    fix toe

--- sources/toe/options.js
+++ sources/toe/options.js
@@ -6,7 +6,26 @@ module.exports = {
 
     dirs: 'dist/svg',
 
-    onSVGDocument: function($svg, item) {
+    onSVGDocument: function($svg) {
+        let found = false;
+        ['path'].forEach((k) => {
+            const $elem = $svg.find(k);
+            const fill = $elem.attr('fill');
+            if (fill && fill !== 'none') {
+                $elem.attr('fill', 'currentColor');
+                found = true;
+            }
+
+            const stroke = $elem.attr('stroke');
+            if (stroke && stroke !== 'none') {
+                $elem.attr('stroke', 'currentColor');
+                found = true;
+            }
+
+        });
+        if (found) {
+            return;
+        }
         $svg.attr('fill', 'currentColor');
     }
 
